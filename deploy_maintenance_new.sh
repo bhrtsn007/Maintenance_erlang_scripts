2e90aabee585043775f3a429d7e5664956cdbb52
bhrtsn007 <bharat.s@greyorange.com>
2020-11-28 13:42:06 +0530

    corrected deploy file

--- deploy_maintenance_new.sh
+++ deploy_maintenance_new.sh
@@ -6,7 +6,7 @@ echo ""
 echo ""
 echo "Renaming file"
 rm -r /home/gor/easy_console/maintenance
-mv /home/gor/easy_console/maintenance_105 /home/gor/easy_console/maintenance
+mv /home/gor/easy_console/maintenance_93 /home/gor/easy_console/maintenance
 echo "making all file executable"
 sudo find /home/gor/easy_console/maintenance -type f -iname "*.sh" -exec chmod +x {} \;
 sudo find /home/gor/easy_console/maintenance -type f -iname "*.escript" -exec chmod +x {} \;
